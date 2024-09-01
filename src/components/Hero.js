636d9879f0f5441de79e61cdb4825679d6bc0873
sbhusal123 <suryabhusal11@gmail.com>
2024-09-01 19:23:58 +0545

    Styles adjustment.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,25 @@
+import { FaPython, FaReact, FaAndroid } from "react-icons/fa";
+
+
 export function Hero(){
     return (
-        <div className="grid sm:grid-cols-2 max-sm:grid-rows-2 items-center sm:p-20">
-            <div>
-                <img  src="story.svg" className="sm:w-100"></img>
-            </div>
+        <div className="relative">
+            <div className="grid md:grid-cols-2 max-sm:grid-rows-2 items-center">
+                <div>
+                    <img  src="story.svg" className="sm:w-100"></img>
+                </div>
 
-            <div className="flex flex-col text-center">
-                <h1 className="text-2xl font-bold">Pariatur eu esse magna</h1>
-                <p className="my-2">
-                    Fugiat do mollit tempor laboris anim reprehenderit dolore voluptate do. Elit proident excepteur officia labore amet amet adipisicing
-                    aliquip quis duis adipisicing labore elit culpa. Eu occaecat est in eiusmod incididunt dolor incididunt velit velit nisi dolore nulla.
-                </p>
-            </div>
-      </div>
+                <div className="flex flex-col text-center relative">
+                    <div>
+                        <h1 className="text-2xl max-sm:text-xl font-bold">Exccellence at You Choice</h1>
+                    </div>
+                    <div className="">
+                        <p className="my-5">
+                            At InfoTech, we are dedicated to transforming your IT vision into reality with unmatched precision and innovation. Our team of experts harnesses cutting-edge technology and best practices to deliver tailored solutions that meet your unique needs. From robust software development to seamless IT infrastructure management, we ensure that every project achieves the highest standards of excellence. Partner with us to experience a blend of technical expertise and personalized service that drives success and fosters growth. Your goals are our priority, and excellence is our commitment.
+                        </p>
+                    </div>
+                </div>
+        </div>
+        </div>
     )
 }
--- src/components/OurWork.js
+++ src/components/OurWork.js
@@ -2,21 +2,27 @@ export function OurWork(){
     return (
         <div id="ourWork">
             <h1 className="text-2xl font-bold text-center mt-10">Our Works</h1> 
-            <div className="flex flex-col sm:flex-row gap-2 px-5 sm:px-15 sm:mx-10 mt-10">
-                <div className="shadow-md p-10 border-slate-600 border-2 rounded-md text-center  hover:shadow-2xl cursor-pointer">
-                    <img src="sample_projects.png" className="rounded-full"/>
+            <div className="flex flex-col sm:flex-row gap-2 sm:px-15 sm:mx-10 mt-10 px-10">
+                <div className="text-center  hover:shadow-2xl cursor-pointer p-2">
+                    <div className="[clip-path:polygon(0%_0%,100%_0%,100%_25%,100%_75%,50%_100%,0%_75%)] relative bg-black-300/15">
+                        <img src="sample_projects.png"/>
+                    </div>
                     <h1 className="font-bold text-center">Aliquip cillum do nostrud</h1>
                     <p className="mt-2 text-sm">Sunt sunt id sint officia ullamco non nulla. Non labore mollit adipisicing enim labore occaecat.</p>
                 </div>
 
-                <div className="shadow-md p-10 border-slate-600 border-2 rounded-md text-center  hover:shadow-2xl cursor-pointer">
-                    <img src="sample_projects.png" className="rounded-full"/>
+                <div className="text-center  hover:shadow-2xl cursor-pointer p-2">
+                <div className="[clip-path:polygon(0%_0%,100%_0%,100%_25%,100%_75%,50%_100%,0%_75%)] relative bg-black-300/15">
+                        <img src="sample_projects.png"/>
+                    </div>
                     <h1 className="font-bold text-center">Aliquip cillum do nostrud</h1>
                     <p className="mt-2 text-sm">Sunt sunt id sint officia ullamco non nulla. Non labore mollit adipisicing enim labore occaecat.</p>
                 </div>
 
-                <div className="shadow-md p-10 border-slate-600 border-2 rounded-md text-center  hover:shadow-2xl cursor-pointer">
-                    <img src="sample_projects.png" className="rounded-full"/>
+                <div className="text-center  hover:shadow-2xl cursor-pointer p-2">
+                <div className="[clip-path:polygon(0%_0%,100%_0%,100%_25%,100%_75%,50%_100%,0%_75%)] relative bg-black-300/15">
+                        <img src="sample_projects.png"/>
+                    </div>
                     <h1 className="font-bold text-center">Aliquip cillum do nostrud</h1>
                     <p className="mt-2 text-sm">Sunt sunt id sint officia ullamco non nulla. Non labore mollit adipisicing enim labore occaecat.</p>
                 </div>
