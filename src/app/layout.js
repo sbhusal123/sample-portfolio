7f1657a1a7f41fb8dfd1c3f50d0669ad54bdd1fa
sbhusal123 <suryabhusal11@gmail.com>
2024-09-01 16:39:14 +0545

    Prod branch test

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>Dev Branch Test</title>
+        <title>Prod Branch Test</title>
       </head>
       <body>
         <NarBar/>
