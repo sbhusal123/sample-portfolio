1f1ec51bec938f2d108cff725d0c3f0fc8684aac
sbhusal123 <suryabhusal11@gmail.com>
2024-09-01 16:35:38 +0545

    CHeck test

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>This is Dev Domain</title>
+        <title>This is Dev</title>
       </head>
       <body>
         <NarBar/>
